57b8f8b18caf897336f3ce928edd2889979f4ea8
Luis Iván García Luna <luisivan@garlun.net>
2015-01-12 12:59:39 -0600

    Styling
    Styling in the HTML and add products to the Store

--- StoreAngular/app.js
+++ StoreAngular/app.js
@@ -1,1 +1,38 @@
-var app = angular.module('store',[]);
+(function(){
+var app = angular.module('store',[ ]);
+
+app.controller('StoreController',function(){
+	this.products = gems;
+});
+
+var gems = [
+{	
+	name : 'Dodecahedron',
+	price: 2.95,
+	description: 'Bla Bla Bla',
+	canPurchase: true,
+	soldOut: false,
+},
+{
+	name: 'Pentagonal Gem',
+	price: 5.95,
+	description: 'Bla Bla Bla Bla... ',
+	canPurchase: true,
+	soldOut: false,
+},
+{
+	name: 'Azurite', 
+	price: 2.95,
+	description: 'Bla Bla...',
+	canPurchase: true,
+	soldOut: false,
+},
+{
+	name: 'Zircon', 
+	price: 3.95,
+	description: 'Bla...',
+	canPurchase: true,
+	soldOut: false,
+},
+]
+})();

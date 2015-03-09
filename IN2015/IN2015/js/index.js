6263b8bddd4e36d13bc4dd054c4ba34c1610aaac
Luis Iván García Luna <luisivan@garlun.net>
2015-03-08 22:14:45 -0600

    Push Push Push, Video

--- IN2015/IN2015/js/index.js
+++ IN2015/IN2015/js/index.js
@@ -1,4 +1,19 @@
-var app = angular.module ("Home",[])
-	app.controller("HomeController",function($scope){
-		$scope.nombre = "Usuario";
+$(function(){
+    var _top = $(window).scrollTop();
+    var _direction;
+    $(window).scroll(function(){
+        var _cur_top = $(window).scrollTop();
+        if(_top < _cur_top)
+        {
+            _direction = 'down';
+          $('.navbar').stop().fadeOut('fast');
+        }
+        else
+        {
+            _direction = 'up';
+          $('.navbar').fadeIn('slow');
+        }
+        _top = _cur_top;
+        console.log(_direction);
+    });
 });

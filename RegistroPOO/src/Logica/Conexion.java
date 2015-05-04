66c8e4031124eb206556dd48f009f35d3294d37b
Luis Iván García Luna <ligl_adobe@hotmail.com>
2015-05-04 11:46:31 -0500

    Created Form

--- RegistroPOO/src/Logica/Conexion.java
+++ RegistroPOO/src/Logica/Conexion.java
@@ -5,10 +5,36 @@
  */
 package Logica;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author LuisIván
  */
 public class Conexion {
-    public String db;
+    public String db = "registrodae";
+    public String url= "jdbc:mysql://127.0.0.1" + db;
+    public String user = "root";
+    public String pass = "";
+
+    public Conexion() {
+    }
+
+    public Connection connectar() {
+        Connection link = null;
+        try {
+            Class.forName("org.gtj.mm.mysql.Driver");
+            link = (Connection) DriverManager.getConnection(this.url,this.user,this.pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+        }
+    return link;
+    }
+    
+
+    
+
 }
